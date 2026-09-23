56262fd6bbe5310e9ba6dee1176ab4b092214dac
Rakshith <rakshithgowda54@gmail.com>
2026-09-23 20:04:35 +0530

    open browser and search google

---
+++ tests/tests/login.spec.ts
@@ -0,0 +1,13 @@
+import { test, expect } from '@playwright/test';
+
+test('search on Google', async ({ page }) => {
+  await page.goto('https://www.google.com');
+
+  await expect(page).toHaveTitle(/Google/);
+
+  await page.getByRole('combobox', { name: /search/i }).fill('Playwright');
+
+  await page.keyboard.press('Enter');
+
+  await expect(page).toHaveTitle(/Playwright/);
+});
